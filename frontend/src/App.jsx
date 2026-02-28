7f3845c044cefe40b26798e93114857b6acf9a68
Shrushti <shrushtikoti234@gmail.com>
2026-02-28 15:02:47 +0530

    report-page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import OrganizationPage from './pages/OrganizationPage'
 import CreateOrganization from './pages/CreateOrganization'
 import CustomizeOrganization from './pages/CustomizeOrganization'
 import Teams from "./pages/Teams";
+import Reports from "./pages/Reports";
 
 function App() {
   const user = typeof window !== 'undefined' ? localStorage.getItem('user') : null
@@ -21,6 +22,7 @@ function App() {
         <Route path="/create" element={<CreateOrganization />} />
         <Route path="/customize" element={<CustomizeOrganization />} />
         <Route path="/teams" element={<Teams />} />
+        <Route path="/reports" element={<Reports />} />
         <Route path="/" element={<Navigate to={user ? '/dashboard' : '/login'} replace />} />
       </Routes>
     </BrowserRouter>
